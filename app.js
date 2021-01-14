82da25222eb8ca874b3610a120149d2e55775ba7
Motasem94 <amotasem.ahmed@gmail.com>
2021-01-14 08:25:27 +0200

    npm i

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
+  // =============================================================================================
 const homeStartingContent = `Lacus vel facilisis volutpat est velit egestas dui id ornare. 
   Semper auctor neque vitae tempus quam. 
   Sit amet cursus sit amet dictum sit amet justo. 
@@ -16,6 +17,7 @@ const homeStartingContent = `Lacus vel facilisis volutpat est velit egestas dui 
   Ultrices vitae auctor eu augue ut lectus arcu bibendum at. 
   Odio euismod lacinia at quis risus sed vulputate odio ut. 
   Cursus mattis molestie a iaculis at erat pellentesque adipiscing.`;
+  // =============================================================================================
 const aboutContent = `Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. 
   Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. 
   Non diam phasellus vestibulum lorem sed. 
@@ -26,6 +28,7 @@ const aboutContent = `Hac habitasse platea dictumst vestibulum rhoncus est pelle
   Amet massa vitae tortor condimentum lacinia quis vel eros. 
   Enim ut tellus elementum sagittis vitae. 
   Mauris ultrices eros in cursus turpis massa tincidunt dui.`;
+  // =============================================================================================
 const contactContent = `Scelerisque eleifend donec pretium vulputate sapien. 
   Rhoncus urna neque viverra justo nec ultrices. 
   Arcu dui vivamus arcu felis bibendum. 
@@ -39,6 +42,11 @@ const contactContent = `Scelerisque eleifend donec pretium vulputate sapien.
   Ultrices gravida dictum fusce ut placerat orci nulla. 
   Mauris in aliquam sem fringilla ut morbi tincidunt. 
   Tortor posuere ac ut consequat semper viverra nam libero.`;
+  // =============================================================================================
+
+  
+
+
 
 const app = express();
 
